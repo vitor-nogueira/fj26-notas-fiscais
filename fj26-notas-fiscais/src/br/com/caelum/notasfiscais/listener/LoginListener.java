f891bc9086f0a90276a515d47697783f8bf3f673
vitor-nogueira <nogueira.vitor@gmail.com>
2017-02-17 14:48:57 -0200

    Injetando dependencias

---
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/listener/LoginListener.java
@@ -0,0 +1,18 @@
+package br.com.caelum.notasfiscais.listener;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import br.com.caelum.notasfiscais.modelo.Usuario;
+import br.com.caelum.notasfiscais.util.EmailComercial;
+
+public class LoginListener {
+	
+	@Inject @EmailComercial
+	private String email;
+	
+	public void onLogin(@Observes Usuario usuario){
+		System.out.printf("Usuario %s se logou no sistema.", usuario.getLogin());
+		System.out.printf("Enviando email para %s ", email);
+	}
+}
